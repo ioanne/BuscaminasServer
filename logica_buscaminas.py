cc4c261cd4fe1406afe8234bd7c2a2490603c8f8
Ioann <ioann@192.168.1.7>
2018-02-20 02:15:27 -0300

    modifique una funcion para generar minas que se utiliza en generar_tablero, ahora tambien agrega las minas a las celdas.

--- logica_buscaminas.py
+++ logica_buscaminas.py
@@ -1,4 +1,6 @@
 from models import Tablero, Fila
+import random
+
 
 def generar_tablero(
             id_partida,
@@ -24,7 +26,33 @@ def generar_tablero(
             db.session.add(fila)
             db.session.commit()
 
-        
+            for x in range(tablero.cantidad_x):
+                celda = Celda(
+                    id_fila=fila.id,
+                    id_tablero=tablero.id,
+                    nro_celda=x
+                )
+                db.session.add(celda)
+                db.session.commit()
 
+        filas = Fila.get_by_id_tablero(tablero.id)
+        minas = generar_minas(
+                    cantidad_minas,
+                    celdas_x,
+                    celdas_y)
+
+        for mina in minas:
+            filas[mina[0]].celdas[mina[1]].contenido = 1
+        db.session.commit()
+        return filas
+    else:
         return tablero
 
+
+def generar_minas(cantidad, celdas_x, celdas_y):
+    contenedor_minas = set()
+    while len(contenedor_minas) <= cantidad -1:
+        x = random.randint(0, celdas_x - 1)
+        y = random.randint(0, celdas_y - 1)
+        contenedor_minas.add((x,y))
+    return contenedor_minas
